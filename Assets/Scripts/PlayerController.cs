cee047f76fda0abc31c0e4cbb3ba5edd0cf778aa
OscarRapale <oscarrapale@gmail.com>
2025-01-19 23:17:34 -0400

    feat: Task 1 added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Public variable to control speed, adjustable in the Inspector
+    public float speed = 5f;
+
+    // Rigidbody component reference
+    private Rigidbody rb;
+
+    void Start()
+    {
+        // Get the Rigidbody component attached to the Player
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // Get input from the horizontal (A/D or Left/Right) and vertical (W/S or Up/Down) axes
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Create a movement vector on the X and Z axes
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Apply force to the Rigidbody to move the Player
+        rb.AddForce(movement * speed);
+    }
+}
